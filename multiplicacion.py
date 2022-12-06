c481b25036a4690f4088c9f1e8bee4dcc0d40d78
EstevanDJ <joseibarra1@misena.edu.co>
2022-12-06 09:08:25 -0500

    Ejercicio de multiplicacino y exponente

---
+++ multiplicacion.py
@@ -0,0 +1,7 @@
+numer = [2]
+nume = int(input("ingrsa el numero"))
+for i in range(1, 5):
+    resulta = nume*i
+    print("el resultado es", resulta)
+    exponente = resulta**2
+    print("el Exponente", exponente)
